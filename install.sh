3444b2accd3cd721cb87d7bc6a1e4f29986b94ad
user <185971707+m35hsn@users.noreply.github.com>
2024-10-22 22:30:54 -0300

    Download and extract AdoptOpenJDKg

---
+++ install.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+#Download and extract AdoptOpenJDKg
+wget "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.24%2B8/OpenJDK11U-jdk_arm_linux_hotspot_11.0.24_8.tar.gz"
+tar -xzvf OpenJDK11U-jdk_arm_linux_hotspot_11.0.24_8.tar.gz
+
