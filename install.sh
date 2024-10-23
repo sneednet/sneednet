7ec7eae76b88e22af7a76acd64058317c433db29
Bjarne <83133360+BjarneVerschorre@users.noreply.github.com>
2024-10-23 21:44:47 +0000

    Update install.sh
    Fixed the broke "Shebang"
    Made the version and filename a variable

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
-#/bin/bash
+#!/bin/bash
+
+version="11.0.24_8"
+filename="OpenJDK11U-jdk_arm_linux_hotspot_${version}.tar.gz"
+known_hash="893ab22f39f102724e7b77b7a06e09b33ee0551b166c0154ce19d4e6abd7e346d0a8314fe2e675e102febf93e001679a1c7c665f6b131c04a453cfbfbabb5003"
 
 echo "Downloading AdoptOpenJDK"
-wget "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.24%2B8/OpenJDK11U-jdk_arm_linux_hotspot_11.0.24_8.tar.gz"
+wget "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.24%2B8/${filename}"
 
-filename="OpenJDK11U-jdk_arm_linux_hotspot_11.0.24_8.tar.gz"
-known_hash="893ab22f39f102724e7b77b7a06e09b33ee0551b166c0154ce19d4e6abd7e346d0a8314fe2e675e102febf93e001679a1c7c665f6b131c04a453cfbfbabb5003"
-echo "Checking hash for file "$filename
+echo "Checking hash for file ${filename}"
 sha=$( shasum -a 512 "$filename" | awk '{ print $1 }')
+
 if [[ $sha == $known_hash ]]
 then
   echo "Hash matches"
@@ -14,8 +17,9 @@ else
   echo $sha "Hash doesn't match, quitting"
   exit
 fi
+
 echo "Exctracting"
-tar -xzvf OpenJDK11U-jdk_arm_linux_hotspot_11.0.24_8.tar.gz
+tar -xzvf $filename
 
 #Download Freenet
 echo "Downloading Freenet jar"
