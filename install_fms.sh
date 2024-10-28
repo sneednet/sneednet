9585387167ed31f39f0a1c85d0e9b63dc752bb8f
user <185971707+m35hsn@users.noreply.github.com>
2024-10-28 19:08:36 -0300

    Apply sane defaults to FMS.

--- install_fms.sh
+++ install_fms.sh
@@ -42,5 +42,38 @@ cp fms_dependencies/usr/lib/i386-linux-gnu/libgomp.so.1.0.0 FMS-install/deps/lib
 cp fms_dependencies/usr/lib/i386-linux-gnu/libIlmThread-2_2.so.12.0.0 FMS-install/deps/libIlmThread-2_2.so.12
 cp fms_dependencies/usr/lib/i386-linux-gnu/libjbig.so.0 FMS-install/deps/libjbig.so.0
 
+sleep 2
+echo "Configuring FMS."
+sleep 2
+
 echo "LD_PRELOAD=./deps/libfreeimage.so.3:./deps/libjxrglue.so.0:./deps/libjpeg.so.8:./deps/libopenjp2.so.7:./deps/libpng16.so.16:./deps/libraw.so.16:./deps/libtiff.so.5:./deps/libwebpmux.so.3:./deps/libwebp.so.6:./deps/libIlmImf-2_2.so.22:./deps/libHalf.so.12:./deps/libIex-2_2.so.12:./deps/libjpegxr.so.0:./deps/libjpeg.so.62:./deps/liblcms2.so.2:./deps/libgomp.so.1:./deps/libIlmThread-2_2.so.12:./deps/libjbig.so.0 ./fms" > FMS-install/run-fms.sh
 chmod +x FMS-install/run-fms.sh
+
+cd FMS-install 
+
+ls
+./run-fms.sh &
+cd ..
+
+sleep 5
+
+echo "Waiting 15s for FMS to finish startup."
+
+sleep 15
+
+echo "Applying settings to FMS."
+
+#Adjust the following settings:  
+#MinLocalMessageTrust 50 0
+#MinPeerMessageTrust 30 0 
+#MinLocalTrustListTrust 50 0
+#MinPeerTrustListTrust 30 0
+#LocalTrustOverridesPeerTrust false true
+#MessageDownloadMaxDaysBackward 5 1000000
+#MessageListDaysBackward 10 1000000
+#ChangeMessageTrustOnReply 0 1
+#DeleteMessagesOlderThan 180 -1
+
+wget -O wizard_form.html http://127.0.0.1:8080/options.htm && sleep 2 && form_password=$(grep -oP 'name=\"formpassword\" value=\"\K[^"]+' wizard_form.html) && wget -O - --post-data="formaction=save&formpassword=$form_password&option%5B23%5D=MinLocalMessageTrust&oldvalue%5B23%5D=50&value%5B23%5D=0&option%5B24%5D=MinPeerMessageTrust&oldvalue%5B24%5D=30&value%5B24%5D=0&option%5B25%5D=MinLocalTrustListTrust&oldvalue%5B25%5D=50&value%5B25%5D=0&option%5B26%5D=MinPeerTrustListTrust&oldvalue%5B26%5D=30&value%5B26%5D=0&option%5B27%5D=LocalTrustOverridesPeerTrust&oldvalue%5B27%5D=false&value%5B27%5D=true&option%5B28%5D=MessageDownloadMaxDaysBackward&oldvalue%5B28%5D=5&value%5B28%5D=1000000&option%5B29%5D=MessageListDaysBackward&oldvalue%5B29%5D=10&value%5B29%5D=1000000&option%5B33%5D=ChangeMessageTrustOnReply&oldvalue%5B33%5D=0&value%5B33%5D=1&option%5B35%5D=DeleteMessagesOlderThan&oldvalue%5B35%5D=180&value%5B35%5D=-1" 'http://127.0.0.1:8080/options.htm' > /dev/null
+
+echo "FMS should be ready to use.".
