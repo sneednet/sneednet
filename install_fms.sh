a46d7976d8acc891379e114966816a5cabd52981
user <185971707+m35hsn@users.noreply.github.com>
2024-10-28 20:38:40 -0300

    Add shared/public identity PublicID to FMS.

--- install_fms.sh
+++ install_fms.sh
@@ -76,4 +76,10 @@ echo "Applying settings to FMS."
 
 wget -O wizard_form.html http://127.0.0.1:8080/options.htm && sleep 2 && form_password=$(grep -oP 'name=\"formpassword\" value=\"\K[^"]+' wizard_form.html) && wget -O - --post-data="formaction=save&formpassword=$form_password&option%5B23%5D=MinLocalMessageTrust&oldvalue%5B23%5D=50&value%5B23%5D=0&option%5B24%5D=MinPeerMessageTrust&oldvalue%5B24%5D=30&value%5B24%5D=0&option%5B25%5D=MinLocalTrustListTrust&oldvalue%5B25%5D=50&value%5B25%5D=0&option%5B26%5D=MinPeerTrustListTrust&oldvalue%5B26%5D=30&value%5B26%5D=0&option%5B27%5D=LocalTrustOverridesPeerTrust&oldvalue%5B27%5D=false&value%5B27%5D=true&option%5B28%5D=MessageDownloadMaxDaysBackward&oldvalue%5B28%5D=5&value%5B28%5D=1000000&option%5B29%5D=MessageListDaysBackward&oldvalue%5B29%5D=10&value%5B29%5D=1000000&option%5B33%5D=ChangeMessageTrustOnReply&oldvalue%5B33%5D=0&value%5B33%5D=1&option%5B35%5D=DeleteMessagesOlderThan&oldvalue%5B35%5D=180&value%5B35%5D=-1" 'http://127.0.0.1:8080/options.htm' > /dev/null
 
-echo "FMS should be ready to use.".
+sleep 2
+echo "Installing PublicID for FMS."
+sleep 3
+
+wget -O wizard_form.html http://127.0.0.1:8080/options.htm && sleep 2 && form_password=$(grep -oP 'name=\"formpassword\" value=\"\K[^"]+' wizard_form.html) && echo "form_password: $form_password" && curl -X POST http://127.0.0.1:8080/localidentities.htm -H 'Content-Type: multipart/form-data' -F "formpassword=$form_password" -F "formaction=import" -F 'file=@public_id.xml'
+
+echo "All done. FMS should be ready to use.".
