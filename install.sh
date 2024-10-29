02387f19f7c3f4f048353b7dd6ce4dd83bda315e
root <185971707+m35hsn@users.noreply.github.com>
2024-10-29 12:19:37 +0000

    Fix headers.

--- install.sh
+++ install.sh
@@ -118,17 +118,14 @@ wget -O - --post-data="formPassword=$form_password&monthlyLimit=999999999&downLi
 echo "Waiting 20s for settings to be applied."
 sleep 20
 
-read -p "Press the Enter key to continue."
-echo "sleep 60"
-sleep 60
-
-curl --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0' 'http://127.0.0.1:8888/CHK@NvT50rE2pz9bne9O-OE~GFY8z-JKul8NoxUDHWv7Evc,yQNgly4kq6WX4pT8A821R4fuOnZUroxi~8vtM1JAehg,AAMC--8/fms-linux-x86-bin-0.3.85.tar.gz' > download_form.html && form_password=$(grep -oP 'name="formPassword"\s*type="hidden"\s*value="\K[^"]+' download_form.html | head -1) && echo "form_password: $form_password" && curl --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0' -X POST 'http://127.0.0.1:8888/downloads/' -H 'Content-Type: multipart/form-data' -F "formPassword=$form_password" -F "key=CHK@NvT50rE2pz9bne9O-OE~GFY8z-JKul8NoxUDHWv7Evc,yQNgly4kq6WX4pT8A821R4fuOnZUroxi~8vtM1JAehg,AAMC--8/fms-linux-x86-bin-0.3.85.tar.gz" -F "return-type=direct" -F "persistence=forever" -F "download=Fetch" -F "filterData=filterData"
+curl 'http://127.0.0.1:8888/CHK@NvT50rE2pz9bne9O-OE~GFY8z-JKul8NoxUDHWv7Evc,yQNgly4kq6WX4pT8A821R4fuOnZUroxi~8vtM1JAehg,AAMC--8/fms-linux-x86-bin-0.3.85.tar.gz' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-User: ?1' > download_form.html && form_password=$(grep -oP 'name="formPassword"\s*type="hidden"\s*value="\K[^"]+' download_form.html | head -1) && echo "form_password: $form_password" && curl --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0' -X POST 'http://127.0.0.1:8888/downloads/' -H 'Content-Type: multipart/form-data' -F "formPassword=$form_password" -F "key=CHK@NvT50rE2pz9bne9O-OE~GFY8z-JKul8NoxUDHWv7Evc,yQNgly4kq6WX4pT8A821R4fuOnZUroxi~8vtM1JAehg,AAMC--8/fms-linux-x86-bin-0.3.85.tar.gz" -F "return-type=direct" -F "persistence=forever" -F "download=Fetch" -F "filterData=filterData"
 
 echo "Waiting 20s for FMS download request to be registered." 
 sleep 20
 
 curl http://127.0.0.1:8888/downloads/ > download_form.html && form_password=$(grep -oP 'name="formPassword"\s*type="hidden"\s*value="\K[^"]+' download_form.html | head -1) && echo "form_password: $form_password" && curl -X POST http://127.0.0.1:8888/downloads/ -H 'Content-Type: multipart/form-data' -F "formPassword=$form_password" -F 'restart_request=Restart' -F 'disableFilterData=disableFilterData' -F 'identifier-0= FProxy:fms-linux-x86-bin-0.3.85.tar.gz' -F 'key-0=freenet:CHK@NvT50rE2pz9bne9O-OE~GFY8z-JKul8NoxUDHWv7Evc,yQNgly4kq6WX4pT8A821R4fuOnZUroxi~8vtM1JAehg,AAMC--8/fms-linux-x86-bin-0.3.85.tar.gz' -F 'filename-0=fms-linux-x86-bin-0.3.85.tar.gz'
 
+echo ""
 echo "Waiting 15s for web interface FMS download to start."
 sleep 15
 
